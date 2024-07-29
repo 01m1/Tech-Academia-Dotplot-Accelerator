26fb29c1a50af2b22c391cc6055a220e57cb09f1
01m1 <69215780+01m1@users.noreply.github.com>
2024-07-29 16:53:02 +0100

    Implement registration

--- frontend/PatientSystem/src/Pages/Registration.jsx
+++ frontend/PatientSystem/src/Pages/Registration.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const url = new URL("http://127.0.0.1:8000/register/");
+    url.searchParams.append("username", username);
+    url.searchParams.append("password", password);
+
+    fetch(url, {
+      method: "POST",
+    }).then((data) => {
+      return data.text();
+    }).then((data) => {
+      if (data === '"Success"') {
+        navigate('/')
+      } else {
+        alert('Error: This account already exists. Please verify your details or use a different email to register.')
+      }
+
+    })
+    .catch((error) => {
+      // Log error if any
+      alert(error);
+      return;
+    });
+  };
   return (
     <div>
       <div className=" flex">
@@ -14,16 +45,20 @@ const Registration = () => {
               type="email"
               placeholder="Email address"
               className="border border-black rounded-lg w-[450px] pl-4 py-[10px]"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
             />
             <input
               type="password"
               placeholder="Password"
               className="border border-black rounded-lg w-[450px] pl-4 py-[10px]"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
           <Link to="/dashboard">
             {" "}
-            <button className="mt-10 py-3 px-12 rounded-3xl text-[14px] bg-[#3E737A] text-white ">
+            <button onClick={handleSubmit} className="mt-10 py-3 px-12 rounded-3xl text-[14px] bg-[#3E737A] text-white ">
               Sign up
             </button>
           </Link>
